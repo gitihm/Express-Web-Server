88b3d555a4e12f5491defc2f00c68d06b0ecee18
gitihm <ismael897@outlook.com>
2019-02-28 16:22:39 +0700

    AIR TEST COM IN LAB

--- index.js
+++ index.js
@@ -9,38 +9,45 @@ app.set('view engine', 'ejs')
 app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 },
    resave : false, saveUninitialized: false }))
 app.use(BodyParser())
-var status =true ;
+var status = true
 app.get('/', (req, res)=>{
-    res.render('form',{status})
+        res.render('form',{status})
+    
+    
+    
  })
 app.get('/admin',(req,res)=>{
-    if(req.session.email){
+    if(req.session.email||req.session.password){
         res.render('admin')
     }else{
-        res.write('<h1>Please login first.</h1>')
-		res.write('<a href="/"><button>login</button></a>')
-		res.end()
+        res.render('loginAgain')
+        status =true
     }
 })
 app.post('/admin', (req, res)=>{
     if(req.body.password!=='240311'){
         status = false
-        res.render('form',{status})
+        res.redirect('/')
     }else{
         status =true
         data= {
-            email : req.body.email
+            email : req.body.email,
+            password : req.body.password
         }
-        req.session.email = req.body.email;
+        req.session.email = req.body.email
+        req.session.password = req.body.password
         res.render('admin',{data})
     }
  })
  app.get('/logout',(req,res)=>{
      req.session.destroy((err)=>{
         if(err) console.log(err)
-        else res.redirect('/')
+        else{
+            res.redirect('/')
+            status = true
+        } 
      })
  })
 app.listen(8080,()=>{
-    console.log("running : 8000")
+    console.log("running : 8080")
 });
